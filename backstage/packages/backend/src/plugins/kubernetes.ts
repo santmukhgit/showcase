4c5ff653b99b46043e8984da348fb6a8122d1f03
Santanu Mukherjee <santanu.61045807@ltimindtree.com>
2024-05-13 12:22:26 +0530

    changes-10 for kubernetes

--- backstage/packages/backend/src/plugins/kubernetes.ts
+++ backstage/packages/backend/src/plugins/kubernetes.ts
@@ -6,43 +6,15 @@ import { PluginEnvironment } from '../types';
 import { CatalogClient } from '@backstage/catalog-client';
 import { Duration } from 'luxon'; 
 
-export class CustomClustersSupplier implements KubernetesClustersSupplier {
-  constructor(private clusterDetails: ClusterDetails[] = []) {}
-
-  static create(refreshInterval: Duration) {
-    const clusterSupplier = new CustomClustersSupplier();
-    // setup refresh, e.g. using a copy of https://github.com/backstage/backstage/blob/master/plugins/kubernetes-backend/src/service/runPeriodically.ts
-    runPeriodically(
-      () => clusterSupplier.refreshClusters(),
-      refreshInterval.toMillis(),
-    );
-    return clusterSupplier;
-  }
-
-  async refreshClusters(): Promise<void> {
-    this.clusterDetails = []; // fetch from somewhere
-  }
-
-  async getClusters(): Promise<ClusterDetails[]> {
-    return this.clusterDetails;
-  }
-}
-
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
  const catalogApi = new CatalogClient({ discoveryApi: env.discovery });
- //const { router } = await KubernetesBuilder.createBuilder({
-const builder = await KubernetesBuilder.createBuilder({
+ const { router } = await KubernetesBuilder.createBuilder({
     logger: env.logger,
     config: env.config,
     catalogApi,
     permissions: env.permissions,
-  //}).build();
-});
-builder.setClusterSupplier(
-  CustomClustersSupplier.create(Duration.fromObject({ minutes: 60 })),
-);
-const { router } = await builder.build();
+  }).build();
   return router;
 } 
