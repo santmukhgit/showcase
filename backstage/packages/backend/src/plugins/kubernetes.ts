af170f23d627a5fbcbd20afa73460531716dcaf5
Santanu Mukherjee <santanu.61045807@ltimindtree.com>
2024-05-13 11:56:39 +0530

    changes-9 for kubernetes

--- backstage/packages/backend/src/plugins/kubernetes.ts
+++ backstage/packages/backend/src/plugins/kubernetes.ts
@@ -1,17 +1,48 @@
-import { KubernetesBuilder } from '@backstage/plugin-kubernetes-backend';
+import { ClusterDetails, 
+         KubernetesBuilder,
+         KubernetesClustersSupplier, } from '@backstage/plugin-kubernetes-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
 import { CatalogClient } from '@backstage/catalog-client';
+import { Duration } from 'luxon'; 
+
+export class CustomClustersSupplier implements KubernetesClustersSupplier {
+  constructor(private clusterDetails: ClusterDetails[] = []) {}
+
+  static create(refreshInterval: Duration) {
+    const clusterSupplier = new CustomClustersSupplier();
+    // setup refresh, e.g. using a copy of https://github.com/backstage/backstage/blob/master/plugins/kubernetes-backend/src/service/runPeriodically.ts
+    runPeriodically(
+      () => clusterSupplier.refreshClusters(),
+      refreshInterval.toMillis(),
+    );
+    return clusterSupplier;
+  }
+
+  async refreshClusters(): Promise<void> {
+    this.clusterDetails = []; // fetch from somewhere
+  }
+
+  async getClusters(): Promise<ClusterDetails[]> {
+    return this.clusterDetails;
+  }
+}
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
  const catalogApi = new CatalogClient({ discoveryApi: env.discovery });
- const { router } = await KubernetesBuilder.createBuilder({
+ //const { router } = await KubernetesBuilder.createBuilder({
+const builder = await KubernetesBuilder.createBuilder({
     logger: env.logger,
     config: env.config,
     catalogApi,
     permissions: env.permissions,
-  }).build();
+  //}).build();
+});
+builder.setClusterSupplier(
+  CustomClustersSupplier.create(Duration.fromObject({ minutes: 60 })),
+);
+const { router } = await builder.build();
   return router;
 } 
