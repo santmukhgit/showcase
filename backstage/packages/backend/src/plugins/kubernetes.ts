8bf9a50d70f6a67f6d5083e927f22a57cfaaaa02
Santanu Mukherjee <santanu.61045807@ltimindtree.com>
2024-05-13 11:14:33 +0530

    changes-V for kubernetes

--- backstage/packages/backend/src/plugins/kubernetes.ts
+++ backstage/packages/backend/src/plugins/kubernetes.ts
@@ -1,4 +1,4 @@
-/* import { KubernetesBuilder } from '@backstage/plugin-kubernetes-backend';
+import { KubernetesBuilder } from '@backstage/plugin-kubernetes-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
 import { CatalogClient } from '@backstage/catalog-client';
@@ -14,4 +14,4 @@ export default async function createPlugin(
     permissions: env.permissions,
   }).build();
   return router;
-} */
+} 
