ea69d96ab0cd6d48adabd4030140b4b01125ccf7
Yuvaraj K <yuvaraj.vinay74@gmail.com>
2024-08-03 18:15:50 +0530

    Fix of Harness Trigger

--- backstage/packages/backend/src/plugins/scaffolder.ts
+++ backstage/packages/backend/src/plugins/scaffolder.ts
@@ -2,6 +2,7 @@ import { CatalogClient } from '@backstage/catalog-client';
 import { createRouter } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import type { PluginEnvironment } from '../types';
+import { createHarnessTriggerAction } from '@internal/backstage-plugin-scaffolder-backend-module-trigger-harness-pipelines';
 
 export default async function createPlugin(
   env: PluginEnvironment,
@@ -9,8 +10,12 @@ export default async function createPlugin(
   const catalogClient = new CatalogClient({
     discoveryApi: env.discovery,
   });
+  const actions = [
+    createHarnessTriggerAction()
+  ];
 
   return await createRouter({
+    actions,
     logger: env.logger,
     config: env.config,
     database: env.database,
